1c5a096fb3f54c951f516ca2ef44fc7b6691269f
SANGWON PARK <eprj453@gmail.com>
2020-05-11 18:22:31 +0900

    S02P31B103-15 #done #comment modify db and checked runserver

--- back/v2020/settings/local.py
+++ back/v2020/settings/local.py
@@ -20,7 +20,7 @@ DATABASES = {
         'USER': 'root', # 데이터베이스 연결시 사용할 유저 이름
         'PASSWORD': 'root@2020', # 유저 패스워드
         'HOST': '127.0.0.1',
-        'PORT': '',
+        'PORT': '0',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
