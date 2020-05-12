05b065710b3831c7fc8b329657f7c4d2f6c99940
gagle637 <gagle637@gmail.com>
2020-05-12 09:40:40 +0900

    S02P31B103-19 #in-progress #comment Add sign up

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -4,4 +4,6 @@ from travels.models import Theme
 # Create your models here.
 
 class User(AbstractUser):
+    username = models.EmailField(blank=False, max_length=254, unique=True)
+    nickname = models.CharField(blank=False, max_length=20, unique=True)
     favorites = models.ManyToManyField(Theme, related_name='like_users') # Theme.like_users.all()
