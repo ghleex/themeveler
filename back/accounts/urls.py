501f6abdaf9b45d3a0a807ed4237899430561a3d
ghleex <ghleex@gmail.com>
2020-05-14 09:26:09 +0900

    S02P31B103-21 #done #comment Checking before merging

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,11 +1,17 @@
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 from django.urls import path
 from . import views
 
 app_name='accounts'
 urlpatterns = [
-    path('user/', views.UserMgmt.as_view()),
+    path('user/', views.SignUp.as_view()),
     path('nickname/', views.Nickname.as_view()),
     path('username/', views.Username.as_view()),
     path('mail/', views.Mail.as_view()),
-    path('travel/<int:pk>/', views.TravelMgmt.as_view()),
+    path('usermgmt/', views.UserMgmt.as_view()),
+    path('password/', views.Password.as_view()),
+    path('travelmgmt/<int:pk>/', views.TravelMgmt.as_view()),
+    path('token/', obtain_jwt_token),
+    path('token/verify/', verify_jwt_token),
+    path('token/refresh/', refresh_jwt_token),
 ]
