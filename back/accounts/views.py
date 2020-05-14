d9dc4f4365e97762fc5e2c091889102a169cbf5b
gagle637 <gagle637@gmail.com>
2020-05-14 14:20:17 +0900

    S02P31B103-49 #done #comment Add ban user and #S02P31B103-20 #done Update sign in

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
+from django.http import HttpRequest 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.utils import jwt_decode_handler
+from rest_framework_jwt.views import obtain_jwt_token
 from .serializers import UserCreationSerializer, UserNicknameSerializer, WaitingSerializer
 from .serializers  import UsernameSerializer, ConfirmCodeSerializer, UserPasswordSerializer
 from .models import Waiting
@@ -15,26 +17,32 @@ from random import SystemRandom
 from datetime import datetime, timedelta, timezone
 import re
 
+
 # Create your views here.
 User = get_user_model()
 decoder = api_settings.JWT_DECODE_HANDLER
 
+def Check_request(request, check_list):
+    data = {}
+    for form in check_list:
+        value = request.get(form)
+        if not value:
+            data[form] = ['이 필드는 필수항목 입니다.']
+        elif form == 'username':
+            p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            if p.match(value) == None:
+                data[form] = ['email형식이 아닙니다.']
+        elif form == 'banning_period' and not value.isdigit():
+            data[form] = ['이 필드는 INT형식이여야 합니다.']
+    return data
+
+
 @permission_classes((AllowAny, ))
 class Mail(APIView):
     def get(self, request, format=None):
-        check_forms = ['username', 'confirm_code']
-        data = {}
-        for form in check_forms:
-            value = request.data.get(form)
-            if not value:
-                data[form] = ['이 필드는 필수항목 입니다.']
-            elif form == 'username':
-                p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-                if p.match(value) == None:
-                    data[form] = ['email형식이 아닙니다.']
-
-        if data:
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        check_result = Check_request(request.data, ['username', 'confirm_code'])
+        if check_result:
+            return Response(check_result, status=status.HTTP_400_BAD_REQUEST)
         if Waiting.objects.filter(username=request.data.get('username')).exists():
             waiting_user = Waiting.objects.get(username=request.data.get('username'))
             if waiting_user.is_confirm:
@@ -50,6 +58,7 @@ class Mail(APIView):
         
         return Response({'message' : ['인증에 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
 
+
     def post(self, request, format=None):
         if UsernameSerializer(data=request.data).is_valid(raise_exception=True):
             confirm_code = ''
@@ -63,12 +72,8 @@ class Mail(APIView):
                 'username' : username,
                 'confirm_code': confirm_code
             }
-            print(Waiting.objects.filter(username=username).exists())
             if Waiting.objects.filter(username=username).exists():
                 waiting_user = Waiting.objects.get(username=username)
-                print()
-                print(waiting_user)
-                print()
                 if waiting_user.username == username:
                     data['is_confirm'] = False
                     serializer = WaitingSerializer(waiting_user, data=data)
@@ -97,6 +102,7 @@ class Mail(APIView):
             return Response({'message' : ['메일 전송에 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message':['이미 가입된 이메일입니다.']},status=status.HTTP_400_BAD_REQUEST)
 
+
 @permission_classes((AllowAny, ))
 class Nickname(APIView):
     def get(self, request, format=None):
@@ -105,6 +111,7 @@ class Nickname(APIView):
             return Response({'message' : ['사용하실수 있는 닉네임입니다.']})
         return Response({'message' : ['닉네임 중복체크를 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
 
+
 @permission_classes((AllowAny, ))
 class Username(APIView):
     def get(self, request, format=None):
@@ -112,6 +119,7 @@ class Username(APIView):
         if serializer.is_valid(raise_exception=True):
             return Response({'message' : ['사용하실수 있는 이메일입니다.']})
         return Response({'message' : ['아이디 중복체크를 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @permission_classes((AllowAny, ))
 class SignUp(APIView):
@@ -129,11 +137,9 @@ class SignUp(APIView):
                         return Response({'message' : ['환영합니다! 회원가입이 정상적으로 처리되었습니다.']})
                 else:
                     return Response({'message' : ['유효시간이 지났습니다.']}, status=status.HTTP_400_BAD_REQUEST)
-                
         else:
             return Response({'message' : ['인증이 되지 않은 회원입니다.']}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message' : ['회원가입이 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
-
 
 class UserMgmt(APIView):
     def delete(self, request, format=None):
@@ -153,7 +159,6 @@ class UserMgmt(APIView):
         return Response({'message' : ['회원정보 변경이 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class Password(APIView):
     def put(self, request, format=None):
         jwt_data = decoder(request.headers['Authorization'].split(' ')[1])
@@ -161,7 +166,6 @@ class Password(APIView):
         password_data = decoder(request.data.get('data'))
         serializer = UserPasswordSerializer(user, data=password_data)
         if serializer.is_valid(raise_exception=True):
-            print(password_data)
             if user.check_password(password_data.get('original_password')):
                 user = serializer.save()
                 user.set_password(password_data.get('password'))
@@ -169,3 +173,42 @@ class Password(APIView):
                 return Response({'message' : ['회원정보가 정상적으로 변경되었습니다.']})
             return Response({'message' : ['비밀번호가 올바르지않습니다.']}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message' : ['비밀번호 변경이 실패하였습니다.']}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@permission_classes((AllowAny, ))
+class SignIn(APIView):
+    def post(self, request, format=None):
+        username=request.data.get("username")
+        if User.objects.filter(username=username).exists():
+            sign_in_user = User.objects.get(username=username)
+            if sign_in_user.banning_period and str(sign_in_user.banning_period) < datetime.today().strftime("%Y-%m-%d"):
+                sign_in_user.is_active = True
+                sign_in_user.banning_period = None
+            sign_in_user.save()
+        return obtain_jwt_token(request._request)
+
+
+@permission_classes((IsAdminUser, ))
+class UserBan(APIView):
+    def post(self, request, format=None):
+        check_result = Check_request(request.data, ['username', 'banning_period',])
+        if check_result:
+            return Response(check_result, status=status.HTTP_400_BAD_REQUEST)
+        username=request.data.get("username")
+        sign_in_user = User.objects.get(username=username)
+        sign_in_user.banning_period = (datetime.today() + timedelta(days=int(request.data.get("banning_period")))).strftime("%Y-%m-%d")
+        sign_in_user.is_active = False
+        sign_in_user.save()
+        return Response({'message' : [username + '가 '+ sign_in_user.banning_period +'까지 정지처리 되었습니다.']})
+
+    def delete(self, request, format=None):
+        check_result = Check_request(request.data, ['username'])
+        if check_result:
+            return Response(check_result, status=status.HTTP_400_BAD_REQUEST)
+        username=request.data.get("username")
+        sign_in_user = User.objects.get(username=username)
+        sign_in_user.banning_period = None
+        sign_in_user.is_active = True
+        sign_in_user.save()
+        return Response({'message' : [username + '의 정지처리가 취소되었습니다.']})
+
