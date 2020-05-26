45f335229a4e422c9ac3049eb17007651e2bc4de
이해인 <koi4553@gmail.com>
2020-05-26 20:03:25 +0900

    S02P31B103-180 S02P31B103-181 S02P31B103-141 S02P31B103-139 S02P31B103-111 #Done

--- front/src/components/SearchBar.vue
+++ front/src/components/SearchBar.vue
@@ -1,24 +1,10 @@
 <template>
-<!-- https://codepen.io/pen/?&editable=true&editors=101=https%3A%2F%2Fvuetifyjs.com%2Fen%2Fcomponents%2Fautocompletes%2F -->
-  <v-toolbar
-    color="rgba(255, 255, 255, 0.85)"
-    class="search-box"
-  >
-  <v-icon>mdi-magnify</v-icon>
+  <!-- https://codepen.io/pen/?&editable=true&editors=101=https%3A%2F%2Fvuetifyjs.com%2Fen%2Fcomponents%2Fautocompletes%2F -->
+  <v-toolbar color="rgba(255, 255, 255, 0.85)" class="search-box">
+    <v-icon>mdi-magnify</v-icon>
     <!-- <v-toolbar-title class="d-none d-sm-none">Search</v-toolbar-title> -->
-    <v-autocomplete
-      v-model="select"
-      :loading="loading"
-      :items="items"
-      :search-input.sync="search"
-      cache-items
-      class="mx-4"
-      flat
-      hide-no-data
-      hide-details
-      label="Where do you want to go?"
-      solo-inverted
-    ></v-autocomplete>
+    <v-autocomplete color="#4DD0E1" v-model="select" :loading="loading" :items="items" :search-input.sync="search" cache-items
+      class="mx-4" flat hide-no-data hide-details label="Where do you want to go?" solo-inverted></v-autocomplete>
     <!-- <v-btn icon> -->
     <v-btn color="#2c3e50" class="text-light">
       검색
@@ -28,26 +14,26 @@
 
 <script>
   export default {
-    data () {
+    data() {
       return {
         loading: false,
         items: [],
         search: null,
         select: null,
         states: [
-         'Spring Season',
-         'Independent Activist',
-         'Timeleap'
+          'Spring Season',
+          'Independent Activist',
+          'Timeleap'
         ],
       }
     },
     watch: {
-      search (val) {
+      search(val) {
         val && val !== this.select && this.querySelections(val)
       },
     },
     methods: {
-      querySelections (v) {
+      querySelections(v) {
         this.loading = true
         // Simulated ajax query
         setTimeout(() => {
@@ -61,9 +47,14 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style>
   .search-box {
-    // width: 50%;
+    /* width: 50%; */
     border-radius: 20px;
+  }
+
+  .search-box .theme--light.v-text-field--solo-inverted.v-input--is-focused>.v-input__control>.v-input__slot {
+    /* background: #4242426b; */
+    background: #2c3e50;
   }
 </style>
