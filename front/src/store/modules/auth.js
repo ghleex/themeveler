0355895594dec074c4aebe25f0c9ada284230587
johh201200 <johh201200@gmail.com>
2020-05-28 11:39:24 +0900

    S02P31B103-182 #done #comment Login,Logout rest api

---
+++ front/src/store/modules/auth.js
@@ -0,0 +1,43 @@
+import jwtDecode from 'jwt-decode'
+
+const state = {
+  token: null
+}
+
+const getters = {
+  isLoggedIn: state => state.token ? true : false,
+  requestHeader(state) {
+    return {
+      headers: {
+        Authorization: `JWT ${state.token}`
+      }
+    }
+  },
+  user(state) {
+    return jwtDecode(state.token).user_id
+  }
+}
+
+// token을 받아와서 state를 update
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+  }
+}
+
+const actions =  {
+  login(context, token) {
+      // mutation 호출 -> commit
+      context.commit('setToken', token)
+  },
+  logout(context) {
+      context.commit('setToken', null)
+  }    
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
