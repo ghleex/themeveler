b60c9cdf2d34473c355ba5f162bd46ff5cd58b39
gagle637 <gagle637@gmail.com>
2020-05-29 14:30:19 +0900

    S02P31B103-200 #in-progress #comment Add social login

--- front/src/router.js
+++ front/src/router.js
@@ -18,6 +18,7 @@ import ProfileTest from './views/Profile/ProfileTest.vue'
 import Travel from './views/Travel.vue'
 // import Map from './views/Map/Map.vue'
 import Error404 from './views/Error404.vue'
+import CheckToken from './views/Social/CheckToken.vue'
 
 Vue.use(VueRouter)
 
@@ -116,7 +117,12 @@ const routes = [
     path: '*',
     name: 'error404',
     component: Error404
-  }
+  },
+  {
+    path: '/checktoken/:nickname/:token',
+    name: 'checkToken',
+    component: CheckToken
+  },
 ]
 
 const router = new VueRouter({
