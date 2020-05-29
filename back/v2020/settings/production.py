97a9ce74e7dbebd045b38928402c258378d963ff
gagle637 <gagle637@gmail.com>
2020-05-29 16:44:01 +0900

    S02P31B103-186 #in-progress #comment Fix sign in

--- back/v2020/settings/production.py
+++ back/v2020/settings/production.py
@@ -31,7 +31,7 @@ DATABASES = {
 
 
 JWT_AUTH = {
-    'JWT_PAYLOAD_HANDLER':'rest_framework_jwt.utils.jwt_payload_handler',
+    'JWT_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_payload_handler',
     'JWT_ENCODE_HANDLER': 'rest_framework_jwt.utils.jwt_encode_handler',
     'JWT_DECODE_HANDLER': 'rest_framework_jwt.utils.jwt_decode_handler',
     'JWT_SECRET_KEY': SECRET_KEY,
