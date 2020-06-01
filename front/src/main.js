1d4d3bdced283f09becb619f84caf332effd9f44
gagle637 <gagle637@gmail.com>
2020-06-01 15:13:40 +0900

    S02P31B103-188 #in-progress #comment Update chatbot

--- front/src/main.js
+++ front/src/main.js
@@ -7,13 +7,19 @@ import store from './store' // vuex
 import VueCsrf from 'vue-csrf'
 import VueSimpleAlert from "vue-simple-alert"
 import axios from 'axios'
+import io from 'socket.io-client'
+const socket = io('http://localhost:3000/')
 
+Vue.prototype.$socket = socket
 Vue.config.productionTip = false
+
 axios.defaults.baseURL = 'http://localhost:8000/api'
 var options = {
   persist: true
 }
+
 Vue.use(VueSession, options)
+Vue.use(require('vue-moment'));
 
 new Vue({
   router,
