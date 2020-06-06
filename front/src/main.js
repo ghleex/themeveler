ca2a50d861e2ed090e81ed74a446390ec703cd72
johh201200 <johh201200@gmail.com>
2020-06-06 23:38:31 +0900

    S02P31B103-213 #in-progress #comment Userpage modify

--- front/src/main.js
+++ front/src/main.js
@@ -5,12 +5,12 @@ import vuetify from './plugins/vuetify'
 import VueSession from 'vue-session'
 import store from './store' // vuex
 import VueCsrf from 'vue-csrf'
-import VueSimpleAlert from "vue-simple-alert"
 import axios from 'axios'
+import VueSimpleAlert from "vue-simple-alert"
 import AOS from 'aos' // fadeIn 이외에 scoll 효과
 import 'aos/dist/aos.css'
 import 'bootstrap/dist/css/bootstrap.min.css' // bootstrap
-// import io from 'socket.io-client' // socket chat
+import io from 'socket.io-client' // socket chat
 
 require('dotenv').config()
 
@@ -21,8 +21,8 @@ axios.defaults.baseURL = process.env.VUE_APP_IP
 var options = {
   persist: true
 }
-// const socket = io(process.env.VUE_APP_SOCKET)
-// Vue.prototype.$socket = socket
+const socket = io(process.env.VUE_APP_SOCKET)
+Vue.prototype.$socket = socket
 
 // fontawsome security policy 참고 (해당 config로 인한 i => svg 태그로 자동 변환, 이에 따른 태그 적용은 index.html head에 유효)
 import fontawesome from '@fortawesome/fontawesome-free'
