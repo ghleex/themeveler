47e45c1803b6c41b88d50d3ea89cec5bc462294f
이해인 <koi4553@gmail.com>
2020-06-07 20:40:50 +0900

    S02P31B103 merge

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -74,24 +74,30 @@
                   <v-slide-group class="pa-4 pop-theme-slide-group" center-active show-arrows>
                     <v-slide-item v-for="(dest, index) in destinations" :key="dest">
                       <v-card class="ma-4 popTheme-sub-img" height="123" width="120">
+                        
                         <!-- 이미지 -->
                         <v-sheet color="#37474F" height="123" @click="openCardModal(dest)">
                           <div class="text-light pb-8" style="font-family: 'Cafe24Simplehae'; font-size: 25px;">
                             #.{{ index+1 }} {{ dest.name }}</div>
                         </v-sheet>
+
                         <v-row class="fill-height" align="center" justify="center">
                         </v-row>
                       </v-card>
                     </v-slide-item>
+
+
                     <!-- modal -->
                     <v-dialog v-model="dialog" width="500">
                       <v-card>
+
                         <!-- 이미지 -->
                         <v-sheet color="#37474F" height="80vh">
                           <div class="text-light pb-8" style="font-family: 'Cafe24Simplehae'; font-size: 25px;">
                             #.{{ dests.id }} {{ dests.name }}
                           </div>
                         </v-sheet>
+                        
                         <v-card-actions style="justify-content: flex-end;">
                           <v-btn color="#2c3e50" class="text-light" @click="dialog = false">확인</v-btn>
                         </v-card-actions>
